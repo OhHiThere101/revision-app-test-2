8bac236b3b6614928c3ceed8c672d0714226573b
EpicCoconutGuy <mjay.jirathanat@gmail.com>
2025-05-28 21:23:16 +0700

    will fix navbar button later

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 const BottomNavbar = () => {
   return (
@@ -7,17 +7,21 @@ const BottomNavbar = () => {
 
         <div className="grid grid-cols-3 h-full">
 
-          <button type="button" className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
 
-            <svg className="w-[32px] h-[32px] text-navigation-text" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-            </svg>
+            <Link href="/home" className="w-full h-full">
 
-            <span className="text-xs text-navigation-text">
-              Home
-            </span>
+              <svg className="w-[32px] h-[32px] text-navigation-text" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
+              </svg>
 
-          </button>
+              <span className="text-xs text-navigation-text">
+                Home
+              </span>
+
+            </Link>
+
+          </div>
 
           <button type="button" className="flex flex-col items-center justify-center">
 
