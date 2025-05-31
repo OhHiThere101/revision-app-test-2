a6c8fcb170aeb2cd4264c1c4d3d9bdd3466049e2
EpicCoconutGuy <mjay.jirathanat@gmail.com>
2025-05-31 23:00:47 +0700

    re-design home page button
    Co-authored-by: ShXn2 <ShXn2@users.noreply.github.com>
    Co-authored-by: OhHiThere101 <OhHiThere101@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,42 +10,58 @@ const HomePage = () => {
 
       <div className="grid gap-4 m-3">
 
-        <div className="bg-gray-800 px-3 py-2 rounded-[6px] shadow-md">
+        <div className="flex flex-row items-center justify-end h-24 bg-[#3A3A40] rounded-[6px] shadow-md">
 
-          <div className="flex flex-row items-center justify-between">
+          <div className="bg-[#2A2930] w-full h-full px-3 py-2 rounded-[6px] shadow-md">
 
-            <div className="">
-              <p className="text-xl font-semibold">Algebra</p>
-              <p className="text-gray-400 text-xs">9th Grade Math</p>
-            </div>
+            <div className="grid grid-cols-[55%_45%] items-center w-full h-full">
 
-            <div className="flex flex-row items-center">
-              <p className="text-green-500 text-md">69% Completed</p>
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" />
-              </svg>
+              <div>
+                <p className="text-xl font-semibold">Algebra</p>
+                <p className="text-gray-400 text-xs">9th Grade Math</p>
+              </div>
 
+              <div className="">
+                <p className="text-green-500 text-md">69% Completed</p>
+                <div className="bg-gray-200 rounded-full h-3 my-[1px] dark:bg-gray-700">
+                  <div className="bg-linear-to-r from-[#58CA88] to-[#BC48FF] h-3 rounded-full w-[69%]"></div>
+                </div>
+              </div>
+              
             </div>
           </div>
+
+          <svg className="w-6 h-6 text-[#5F5F63]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="m9 5 7 7-7 7" />
+          </svg>
+
         </div>
 
-        <div className="bg-gray-800 px-3 py-2 rounded-[6px] shadow-md">
+        <div className="flex flex-row items-center justify-end h-24 bg-[#3A3A40] rounded-[6px] shadow-md">
 
-          <div className="flex flex-row items-center justify-between">
+          <div className="bg-[#2A2930] w-full h-full px-3 py-2 rounded-[6px] shadow-md">
 
-            <div className="">
-              <p className="text-xl font-semibold">Work & Energy</p>
-              <p className="text-gray-400 text-xs">9th Grade Physics</p>
-            </div>
+            <div className="grid grid-cols-[55%_45%] items-center w-full h-full">
 
-            <div className="flex flex-row items-center">
-              <p className="text-green-500 text-md">7% Completed</p>
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" />
-              </svg>
+              <div>
+                <p className="text-xl font-semibold">Negromancy</p>
+                <p className="text-gray-400 text-xs">9th Grade Math</p>
+              </div>
 
+              <div>
+                <p className="text-green-500 text-md">13% Completed</p>
+                <div className="bg-gray-200 rounded-full h-3 my-[1px] dark:bg-gray-700">
+                  <div className="bg-linear-to-r from-[#58CA88] to-[#BC48FF] h-3 rounded-full w-[69%]"></div>
+                </div>
+              </div>
+              
             </div>
           </div>
+
+          <svg className="w-6 h-6 text-[#5F5F63]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="m9 5 7 7-7 7" />
+          </svg>
+
         </div>
 
       </div>
